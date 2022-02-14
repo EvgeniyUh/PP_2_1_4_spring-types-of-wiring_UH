85e5f372973a3db6db7b810ac7247fdad8c4dc0b
EvgeniyUh <uhevg@mail.ru>
2022-02-14 18:48:57 +0300

    214

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -5,6 +5,8 @@ import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import koschei.models.Egg6;
+import koschei.models.Needle7;
 
 @Configuration
 @ComponentScan(basePackages = "koschei")
@@ -14,4 +16,9 @@ public class AppConfig {
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+    @Bean
+    public static Egg6 getEgg(Needle7 needle) {
+        return new Egg6(needle);
+    }
+
 }
